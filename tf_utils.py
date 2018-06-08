808b96e61f0344910d6f1913c677b5773c14180a
venf2k <venf2k@yahoo.it>
2018-06-08 10:03:43 +0200

    Add files via upload

--- tf_utils.py
+++ tf_utils.py
@@ -11,6 +11,7 @@ import numpy as np
 import tensorflow as tf
 import math
 
+
 def load_dataset():
 #
 # load, split & shuffle 40.000 tuples dataset
@@ -136,3 +137,70 @@ def forward_propagation_for_predict(X, parameters):
     
     return Z3
     
+
+def load_sample_dataset():
+#
+# load, split & shuffle 40.000 tuples dataset
+#
+    Dataset = h5py.File('data/dataset_full_5000.hdf5', "r")
+    #list(Dataset.keys())
+    X_Dataset_orig = np.array(Dataset["img"][:]) # your 40.000 train set features: shape = (8 x 5.000,100,100,3)
+    Y_Dataset_orig = np.array(Dataset["labels"][:], int)
+    Y_Dataset_orig = Y_Dataset_orig.reshape((1, Y_Dataset_orig.shape[0]))
+    #Shuffle (X, Y)
+    seed=0
+    np.random.seed(seed)            # To make your "random" minibatches the same as ours
+    m = X_Dataset_orig.shape[0]                  # number of training examples
+    permutation = list(np.random.permutation(m))
+    X_Dataset_shuffled = X_Dataset_orig[permutation, :]
+    Y_Dataset_shuffled = Y_Dataset_orig[:, permutation]
+    X_Dataset_train = X_Dataset_shuffled[:2800,:]
+    Y_Dataset_train = Y_Dataset_shuffled[:,:2800]
+    X_Dataset_test = X_Dataset_shuffled[2800:3000,:]
+    Y_Dataset_test = Y_Dataset_shuffled[:,2800:3000]
+    classes = np.array(range(7), int)
+    return X_Dataset_train, Y_Dataset_train, X_Dataset_test, Y_Dataset_test, classes
+
+
+def load_sample_class_dataset(nclass = 0):
+#
+# load, split & shuffle 40.000 tuples dataset
+#
+    Dataset = h5py.File('data/dataset_full_5000.hdf5', "r")
+    #list(Dataset.keys())
+    X_Dataset_orig = np.array(Dataset["img"][:]) # your 40.000 train set features: shape = (8 x 5.000,100,100,3)
+    Y_Dataset_orig = np.array(Dataset["labels"][:], int)
+    Y_Dataset_orig = Y_Dataset_orig.reshape((1, Y_Dataset_orig.shape[0]))
+    #Extract class (X, Y)
+    X_Dataset_train = X_Dataset_orig[nclass*5000:nclass*5000+750,:]
+    Y_Dataset_train = Y_Dataset_orig[:,nclass*5000:nclass*5000+750]
+    X_Dataset_test = X_Dataset_orig[nclass*5000+750:nclass*5000+1000,:]
+    Y_Dataset_test = Y_Dataset_orig[:,nclass*5000+750:nclass*5000+1000]
+    classes = np.array(range(7), int)
+    return X_Dataset_train, Y_Dataset_train, X_Dataset_test, Y_Dataset_test, classes
+
+
+def show_images(images, cols = 3, titles = None):
+    """Display a list of images in a single figure with matplotlib.
+    Parameters
+    ---------
+    images: List of np.arrays compatible with plt.imshow.
+    
+    cols (Default = 1): Number of columns in figure (number of rows is 
+                        set to np.ceil(n_images/float(cols))).
+    
+    titles: List of titles corresponding to each image. Must have
+            the same length as titles.
+    """
+    assert((titles is None)or (len(images) == len(titles)))
+    n_images = len(images)
+    if titles is None: titles = ['Image (%d)' % i for i in range(1,n_images + 1)]
+    fig = plt.figure()
+    for n, (image, title) in enumerate(zip(images, titles)):
+        a = fig.add_subplot(cols, np.ceil(n_images/float(cols)), n + 1)
+        if image.ndim == 2:
+            plt.gray()
+        plt.imshow(image)
+        a.set_title(title)
+    fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
+    plt.show()
